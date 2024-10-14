0fde0031b90cf3ecef3d9c9c7a1f9c0c6f11c32a
shine00chang <shine00chang@gmail.com>
2024-10-14 02:54:45 +0000

    (fix) ranklist

--- packages/common/src/services/team.services.ts
+++ packages/common/src/services/team.services.ts
@@ -2,21 +2,40 @@ import { teamScoreCollection } from '../connections/mongodb.connections.js'
 
 export async function recalculateTeamTotalScore ({ contestId, teamId = undefined }: { contestId: string, teamId?: string }): Promise<void> {
   const query = teamId != null ? { contestId, id: teamId } : { contestId }
-  await teamScoreCollection.updateMany(query, [
-    {
-      $project: { scores: { $objectToArray: '$scores' } }
-    },
-    {
-      $set: { totalScore: { $sum: '$scores.v' } }
-    }
-  ])
 
-  await teamScoreCollection.updateMany(query, [
+  await teamScoreCollection.aggregate([
     {
-      $project: { time: { $objectToArray: '$time' } }
+      $match: query
     },
     {
-      $set: { lastTime: { $max: '$time.v' } }
+      $project: { v: { $objectToArray: '$scores' } }
+    },
+    {
+      $set: { totalScore: { $sum: '$v.v' } }
+    },
+    {
+      $unset: "v"
+    },
+    { 
+      $merge: { into: "teamScores" } 
     }
-  ])
+  ]).toArray()
+
+  await teamScoreCollection.aggregate([
+    {
+      $match: query
+    },
+    {
+      $project: { v: { $objectToArray: '$time' } }
+    },
+    {
+      $set: { lastTime: { $max: '$v.v' } }
+    },
+    {
+      $unset: "v"
+    },
+    { 
+      $merge: { into: "lastTime" } 
+    }
+  ]).toArray()
 }
